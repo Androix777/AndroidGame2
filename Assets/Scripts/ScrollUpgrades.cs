f60704d1f5182e6bea3da2361562a059adbd7e05
Unknown <androix77@gmail.com>
2018-09-15 10:23:02 +0300

    Добавлен пример скролла

---
+++ Assets/Scripts/ScrollUpgrades.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUpgrades : MonoBehaviour {
+
+	GameObject content;
+	GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+		content = gameObject.transform.Find("Viewport").Find("Content").gameObject;
+		panel = content.transform.Find("PanelExample").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void AddPanel () {
+		
+	}
+}
